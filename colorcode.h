ce7c96f42ea55219e09651ded7b6f9d045787f39
qei1cob <qei1cob@bosch.com>
2025-08-12 11:03:20 +0530

    [Modularity]-Divide into files changes
    [Modularity]-Divide into files changes

---
+++ colorcode.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define MAX_COLORPAIR_NAME_CHARS 16
+
+// Enums
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+// Struct
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+// Extern variables
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+
+// Functions
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
